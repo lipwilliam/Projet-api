c909b2a0fc031ca77906980f519031317c671a68
Jo-Philippe William <97845487+lipwilliam@users.noreply.github.com>
2023-02-17 16:47:23 +0100

    Update Weather.js

--- Weather.js
+++ Weather.js
@@ -1,6 +1,6 @@
 class Weather {
     async getCurrent(input) {
-        const myKey = '6e6ec156ddc0235d735d505f2394db36'
+        const myKey = ''
 
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${myKey}&units=metric`)
         const data = await response.json()
